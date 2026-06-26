48ae5916342dc9ebb58dbe3c92f3c1796f339554
Rugved Sitafale <rugvedsitafale1014@gmail.com>
2026-06-26 21:28:23 +0530

    Sync LeetCode solution: Add Two Integers (C++)

---
+++ Easy/2383_Add_Two_Integers.cpp
@@ -0,0 +1,18 @@
+/*
+ * Problem: Add Two Integers
+ * Problem ID: 2383
+ * Difficulty: Easy
+ * Language: C++
+ * Runtime: 0 ms
+ * Memory: 8.2 MB
+ * Synced From: LeetCode
+ * Date: 2026-06-26
+ */
+
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        return num1+num2;
+        
+    }
+};
