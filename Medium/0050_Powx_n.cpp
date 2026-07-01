dcd5cbc30b55db41c3bf517109f4f824aa231728
Rugved Sitafale <rugvedsitafale1014@gmail.com>
2026-07-01 08:45:15 +0530

    Sync LeetCode solution: Pow(x, n) (C++)

---
+++ Medium/0050_Powx_n.cpp
@@ -0,0 +1,20 @@
+/*
+ * Problem: Pow(x, n)
+ * Problem ID: 50
+ * Difficulty: Medium
+ * Language: C++
+ * Runtime: 0 ms
+ * Memory: 8.9 MB
+ * Synced From: LeetCode
+ * Date: 2026-07-01
+ */
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double y=pow(x,n);
+
+        return y;
+        
+    }
+};
