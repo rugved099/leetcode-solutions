4c08804fc2a881ceb43bdfeb166fb9bb9cfd19c3
Rugved Sitafale <rugvedsitafale1014@gmail.com>
2026-07-07 21:20:28 +0530

    Sync LeetCode solution: Concatenate Non-Zero Digits and Multiply by Sum I (C++)

---
+++ Easy/4135_Concatenate_Non-Zero_Digits_and_Multiply_by_Sum_I.cpp
@@ -0,0 +1,33 @@
+/*
+ * Problem: Concatenate Non-Zero Digits and Multiply by Sum I
+ * Problem ID: 4135
+ * Difficulty: Easy
+ * Language: C++
+ * Runtime: 0 ms
+ * Memory: 9 MB
+ * Synced From: LeetCode
+ * Date: 2026-07-07
+ */
+
+class Solution {
+public:
+    long long sumAndMultiply(int n) {
+        long long sum = 0;
+        long long num = 0;
+        long long place = 1;
+
+        while (n > 0) {
+            int rem = n % 10;
+
+            if (rem != 0) {
+                sum += rem;
+                num = rem * place + num;
+                place *= 10;
+            }
+
+            n /= 10;
+        }
+
+        return num * sum;
+    }
+};
