e82a1f46d010e9064821addf44d3151f52054a08
Rugved Sitafale <rugvedsitafale1014@gmail.com>
2026-07-17 22:55:19 +0530

    Sync LeetCode solution: Sequential Digits (C++)

---
+++ Medium/1212_Sequential_Digits.cpp
@@ -0,0 +1,36 @@
+/*
+ * Problem: Sequential Digits
+ * Problem ID: 1212
+ * Difficulty: Medium
+ * Language: C++
+ * Runtime: 0 ms
+ * Memory: 8.4 MB
+ * Synced From: LeetCode
+ * Date: 2026-07-17
+ */
+
+class Solution {
+public:
+    vector<int> sequentialDigits(int low, int high) {
+
+        vector<int> all = {
+            12,23,34,45,56,67,78,89,
+            123,234,345,456,567,678,789,
+            1234,2345,3456,4567,5678,6789,
+            12345,23456,34567,45678,56789,
+            123456,234567,345678,456789,
+            1234567,2345678,3456789,
+            12345678,23456789,
+            123456789
+        };
+
+        vector<int> ans;
+
+        for (int x : all) {
+            if (x >= low && x <= high)
+                ans.push_back(x);
+        }
+
+        return ans;
+    }
+};
