1ac740c246f256c4b28e8284e2c1449cec1266fe
Rugved Sitafale <rugvedsitafale1014@gmail.com>
2026-08-03 19:50:37 +0530

    Sync LeetCode solution: Stone Game (C++)

---
+++ Medium/0909_Stone_Game.cpp
@@ -0,0 +1,17 @@
+/*
+ * Problem: Stone Game
+ * Problem ID: 909
+ * Difficulty: Medium
+ * Language: C++
+ * Runtime: 0 ms
+ * Memory: 10.4 MB
+ * Synced From: LeetCode
+ * Date: 2026-08-03
+ */
+
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    }
+};
