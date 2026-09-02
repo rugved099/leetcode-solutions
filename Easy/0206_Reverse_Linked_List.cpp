ba105a57ac4888898394217e9adf3c8a9d3b4cfd
Rugved Sitafale <rugvedsitafale1014@gmail.com>
2026-09-02 17:12:20 +0530

    Sync LeetCode solution: Reverse Linked List (C++)

---
+++ Easy/0206_Reverse_Linked_List.cpp
@@ -0,0 +1,37 @@
+/*
+ * Problem: Reverse Linked List
+ * Problem ID: 206
+ * Difficulty: Easy
+ * Language: C++
+ * Runtime: 0 ms
+ * Memory: 13.3 MB
+ * Synced From: LeetCode
+ * Date: 2026-09-02
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode*p=head;
+        ListNode*q=NULL;
+        ListNode*r=NULL;
+
+        while(p!=NULL){
+            r=q;
+            q=p;
+            p=p->next;
+            q->next=r;
+        }
+        return q;
+    }
+};
