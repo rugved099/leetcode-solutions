13693b543c35bf5a3bdd3da18b8ae7fbac9366dc
Rugved Sitafale <rugvedsitafale1014@gmail.com>
2026-09-08 10:26:47 +0530

    Sync LeetCode solution: Linked List Cycle II (C++)

---
+++ Medium/0142_Linked_List_Cycle_II.cpp
@@ -0,0 +1,44 @@
+/*
+ * Problem: Linked List Cycle II
+ * Problem ID: 142
+ * Difficulty: Medium
+ * Language: C++
+ * Runtime: 6 ms
+ * Memory: 11.3 MB
+ * Synced From: LeetCode
+ * Date: 2026-09-08
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(head==nullptr || head->next==nullptr) return 0;
+
+        ListNode*fast=head;
+        ListNode*slow=head;
+        ListNode*fixed;
+        while(fast!=nullptr && fast->next!=nullptr){
+            fast=fast->next->next;
+            slow=slow->next;
+            if(fast==slow){
+                fixed=slow;
+                fast=head;
+                while(fast!=slow){
+                    fast=fast->next;
+                    slow=slow->next;
+                }
+                return fast;
+            }
+        }
+        
+        return nullptr;
+    }
+};
